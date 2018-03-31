80fc620826817817c64f38c9312c23b2b1b4d89a
Piotr Polak <piotr@polak.ro>
2018-03-31 14:31:07 +0200

    Updated composer setup scripts

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 PEPIS_CMS_DATABASE_CONFIG_TYPE=native
-PEPIS_CMS_DATABASE_HOSTNAME=db
-PEPIS_CMS_DATABASE_USERNAME=pepiscms
-PEPIS_CMS_DATABASE_PASSWORD=pepiscms
-PEPIS_CMS_DATABASE_DATABASE=pepiscms
+#PEPIS_CMS_DATABASE_HOSTNAME=db
+#PEPIS_CMS_DATABASE_USERNAME=pepiscms
+#PEPIS_CMS_DATABASE_PASSWORD=pepiscms
+#PEPIS_CMS_DATABASE_DATABASE=pepiscms
 PEPIS_CMS_AUTH_DRIVER=native
 PEPIS_CMS_AUTH_EMAIL=piotr@polak.ro
 PEPIS_CMS_AUTH_PASSWORD=demodemo
 PEPIS_CMS_SITE_EMAIL=piotr@polak.ro
 PEPIS_CMS_SITE_NAME=Demonstration
 
-composer install --prefer-dist && \
-    echo "Composer dependencies installed" &&\
-    cp vendor/piotrpolak/pepiscms/pepiscms/resources/config_template/template_index.php ./index.php && \
+cp vendor/piotrpolak/pepiscms/pepiscms/resources/config_template/template_index.php ./index.php && \
     echo "index.php created" && \
     sed -i -e 's/TEMPLATE_VENDOR_PATH/\.\/vendor\//g' ./index.php && \
     echo "vendor path adjusted" && \
